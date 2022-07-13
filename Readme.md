0e9f19469b6269cfa116dc54873faf9175c4a60b
Islam Mohamed <islammohamedd1@gmail.com>
2022-07-13 20:10:18 +0200

    colab utils

---
+++ Readme.md
@@ -0,0 +1,66 @@
+# Colab Project Utils
+
+Utility class to help you save your colab project files in your Google Drive account and fetch them back when you continue working on you project.
+
+The goal of this project is to save you time while working on complex projects using Colab.
+
+## Getting Started
+
+#### 1. Mount Google Drive
+
+```python
+DRIVE_PATH = '/gdrive'
+```
+
+```python
+from google.colab import drive
+drive.mount(DRIVE_PATH)
+```
+
+    Drive already mounted at /gdrive; to attempt to forcibly remount, call drive.mount("/gdrive", force_remount=True).
+
+#### 2. Download and import ColabUtils
+
+```python
+!wget https://raw.githubusercontent.com/islammohamedd1/colab-project-utils/main/colab_utils.py
+```
+
+```python
+from colab_utils import ColabUtils
+```
+
+#### 3. Initialize ColabUtils
+
+When you initialize ColabUtils, it will look if the project already exists in your Google Drive using the following path
+`{drive_root}/{project_root}/{project_name}`
+If the project exists, it will copy all the files to the current session storage.
+
+```python
+PROJECT_NAME = "Test Project"
+PROJECT_ROOT = 'colab_projects'
+
+cu = ColabUtils(PROJECT_NAME, DRIVE_PATH, PROJECT_ROOT)
+```
+
+    Initialized project Test Project using the following path: /gdrive/MyDrive/colab_projects/Test Project
+    Project does not exist, creating new project
+    Created project Test Project at /gdrive/MyDrive/colab_projects/Test Project
+
+#### 4. Write a test file
+
+```python
+%%writefile test.py
+print('hello world')
+```
+
+    Writing test.py
+
+#### 5. Save the project
+
+```python
+cu.save_project()
+```
+
+    Saved project Test Project
+
+Voila! Your project is now saved to your Google Drive account. When you start working on you project again, the files will be fetched automatically when you initialize the ColabUtils object as described in step 3
---
+++ colab_utils.py
@@ -0,0 +1,132 @@
+import os
+import shutil
+
+
+class ColabUtils:
+    """
+    Colab Utils is a utility class that helps you to save the files in your project's session storage to you Google Drive account.
+    If you have any modules and/or data files in you working directory and don't want to lose them when your runtime resets,
+    you can save the files to your Google Drive account and fetch them back when you restart your runtime to the current session storage.
+    """
+
+    __default_files = [
+        "sample_data",
+        ".config",
+        ".ipynb_checkpoints",
+        "__pycache__",
+    ]
+
+    def __init__(
+        self, project_name: str, drive_root: str, project_drive_root: str
+    ):
+        """
+        Initializes the ColabUtils class.
+
+        Parameters:
+            project_name: The name of the project. This name will be used to name the project directory
+            drive_root: The root directory of your Google Drive account.
+            project_drive_root: The parent directory of your project in google drive. The project will be saved in the following path: drive_root/project_drive_root/project_name
+        """
+        self.project_name = project_name
+        self.project_drive_root = project_drive_root
+        self.drive_root = self.__load_drive_root_path(drive_root)
+        self.__project_path = os.path.join(
+            self.drive_root, project_drive_root, project_name
+        )
+
+        print(
+            f"Initialized project {self.project_name} using the following path: {self.__project_path}"
+        )
+
+        self.fetch_project()
+
+    def __load_drive_root_path(self, drive_root: str):
+        """
+        Loads the drive root path.
+
+        Parameters:
+            drive_root: The drive root path.
+        """
+        if not os.path.exists(drive_root) or not os.path.isdir(drive_root):
+            raise FileNotFoundError(
+                "The drive root path does not exist. Please check the path and try again."
+            )
+
+        if "MyDrive" not in drive_root:
+            drive_root = os.path.join(drive_root, "MyDrive")
+
+        return drive_root
+
+    def __is_drive_root(self, file: str):
+        """
+        Checks if the file path is the drive root directory. Prevents recursive copying of files.
+
+        Parameters:
+            file: The file path to check.
+        """
+        if file == self.drive_root or os.path.abspath(file) == os.path.abspath(
+            self.drive_root
+        ):
+            return True
+
+    def __copy_files(self, files: list, dest: str):
+        """
+        Copies the files to the destination.
+        """
+        for f in files:
+            filename = os.path.basename(f)
+            # check if the file exists
+            if os.path.exists(os.path.join(dest, filename)):
+                continue
+            shutil.copy(f, os.path.join(dest, filename))
+
+    def save_project(self, ignore_files: list = None):
+        """
+        Saves the project to your Google Drive account.
+
+        Parameters:
+            ignore_files: A list of files to ignore when saving the project.
+            The following list is excluded by default: ['sample_data', '.config', '.ipynb_checkpoints', "__pycache__"]
+        """
+        if not os.path.exists(self.__project_path):
+            os.makedirs(self.__project_path)
+
+        if ignore_files is None:
+            ignore_files = []
+
+        ignore_file_list = self.__default_files + ignore_files
+        files = os.listdir(".")
+        files = [
+            f
+            for f in files
+            if f not in ignore_file_list and not self.__is_drive_root(f)
+        ]
+
+        if len(files):
+            self.__copy_files(files, self.__project_path)
+
+        print(f"Saved project {self.project_name} at {self.__project_path}")
+
+    def fetch_project(self):
+        """
+        Fetches the project from your Google Drive account.
+        """
+
+        if os.path.exists(self.__project_path):
+            print(f"Project already exists, fetching files")
+
+            files = os.listdir(self.__project_path)
+            files = [os.path.join(self.__project_path, f) for f in files]
+
+            if len(files):
+                self.__copy_files(files, ".")
+
+            print(f"Fetched project {self.project_name}")
+        else:
+            print(f"Project does not exist, creating new project")
+
+            os.makedirs(self.__project_path)
+
+            print(
+                f"Created project {self.project_name} at {self.__project_path}"
+            )
